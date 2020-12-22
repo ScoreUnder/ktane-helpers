8e381e8e9bec679e5409b7d2c3594f7770909742
Score_Under <seejay.11@gmail.com>
2020-12-22 00:11:10 +0000

    Make better use of batteries in passwords.ml

--- passwords.ml
+++ passwords.ml
@@ -2,16 +2,10 @@ open Batteries
 
 let passwords = File.lines_of "passwords.txt" |> List.of_enum
 
-let read_lists f =
-  let rec aux acc =
-    let line = try input_line stdin with End_of_file -> "" in
-    if line = "" then acc
-    else
-      let next = line :: acc in
-      f next;
-      aux next
-  in
-  aux []
+let read_lists =
+  IO.lines_of stdin
+  |> Enum.filter (( <> ) "")
+  |> Enum.scanl (fun acc line -> line :: acc) []
 
 let print_list ls = Printf.printf "[%s]\n%!" (String.concat ", " ls)
 
@@ -20,4 +14,4 @@ let filter_passwords filters =
     (fun i input acc -> List.filter (fun v -> String.contains input v.[i]) acc)
     (List.rev filters) passwords
 
-let () = read_lists (fun ls -> print_list (filter_passwords ls)) |> ignore
+let () = read_lists |> Enum.iter (filter_passwords %> print_list)
